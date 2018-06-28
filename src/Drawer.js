f513efab0fc910f48bafe206e8da7e830d0c15eb
jn9cn <jannine.chan@gmail.com>
2018-06-28 14:19:33 -0400

    removed comments

--- src/Drawer.js
+++ src/Drawer.js
@@ -4,22 +4,20 @@ import data from './data.js';
 
 class Drawer extends Component {
   constuctor() {
+    // eslint-disable-next-line 
     this.state = {
       name: '',
       communities: [],
-      sortName: null, // null, asc, desc
+      sortName: null, // null, 'asc', 'desc'
       sortCase: null,
     }
     this.compareNames = this.compareNames.bind(this)
     this.compareCases = this.compareCases.bind(this)
     this.sortNames = this.sortNames.bind(this)
     this.sortCases = this.sortCases.bind(this)
-    this.handleNameClick = this.handleNameClick.bind(this)
-    this.handleCaseClick = this.handleCaseClick.bind(this)
   }
 
   componentWillMount() {
-      console.log('comp will mount')
     this.setState({
         name: data.name, 
         communities: data.communities,
@@ -29,7 +27,6 @@ class Drawer extends Component {
   }
 
   componentDidMount() {
-      console.log('comp did mount')
     this.setState({
         name: data.name, 
         communities: data.communities
@@ -37,16 +34,13 @@ class Drawer extends Component {
   }
   
   compareNames(name) {
-    //   console.log("compareNames", this.state.communities, "sortName", this.state.sortName)
     if(this.state.sortName === 'asc') {
-        // console.log("compareNames asc")
         return (a,b) => {
             if(a[name] < b[name]) return 1
             if(a[name] > b[name]) return -1
             return 0
         }
     } else {
-        // console.log("compareNames desc")
         return (a,b) => {
             if(a[name] < b[name]) return -1
             if(a[name] > b[name]) return 1
@@ -56,68 +50,40 @@ class Drawer extends Component {
   }
 
   compareCases(num) {
-    //   console.log("compareCases", this.state.communities)
     if(this.state.sortCase === 'asc') {
-        // console.log("compareCases asc")
-        return function (a,b) { return b[num] - a[num] }
+        return (a,b) => b[num] - a[num] 
     } else {
-        // console.log("compareCases desc")
-        return function (a,b) { return a[num] - b[num] }
+        return (a,b) => a[num] - b[num]
     }
   }
 
   sortNames(key) {
-      let arrCopy = [...this.state.communities]
-    //   console.log("sn b4", arrCopy)
-      return arrCopy.sort(this.compareNames(key))
-    //   console.log("sn aft", arrCopy)
-    //   this.setState({communities: arrCopy})
+    let arrCopy = [...this.state.communities]
+    return arrCopy.sort(this.compareNames(key))
   }
 
   sortCases(key) {
     let arrCopy = [...this.state.communities]
-    // console.log("sc b4", arrCopy)
     return arrCopy.sort(this.compareCases(key))
-    // console.log("sc aft", arrCopy)
-    // this.setState({communities: arrCopy})
   }
 
   handleNameClick() {
-    //   console.log("on name click", this.state.sortName)
-      if (this.state.sortName === null || this.state.sortName === 'desc') {
-        //   console.log("now sn is null or desc -->", this.state.sortName)
-          this.setState({communities: this.sortNames('name'), sortName: 'asc', sortCase: null})
-        //   console.log("now sn is asc -->", this.state.sortName)
-        //   this.sortNames('name')
-      } else if (this.state.sortName === 'asc') {
-        // console.log("now sn is asc -->", this.state.sortName)
+    if(this.state.sortName === null || this.state.sortName === 'desc') {
+        this.setState({communities: this.sortNames('name'), sortName: 'asc', sortCase: null})
+    } else if(this.state.sortName === 'asc') {
         this.setState({communities: this.sortNames('name'), sortName: 'desc', sortCase: null})
-        // console.log("now sn is desc -->", this.state.sortName)
-        //   this.sortNames('name')
-      }
+    }
   }
 
   handleCaseClick() {
-    // console.log("on case click", this.state.sortCase)
-    if (this.state.sortCase === null || this.state.sortCase === 'desc') {
-        // console.log("now sc is nul or desc -->", this.state.sortCase)
+    if(this.state.sortCase === null || this.state.sortCase === 'desc') {
         this.setState({communities: this.sortCases('cases'), sortName: null, sortCase: 'asc'})
-        // console.log("now sc is asc -->", this.state.sortCase)
-        // this.sortCases('cases')
-    } else if (this.state.sortCase === 'asc') {
-    //   console.log("now sc is asc -->", this.state.sortCase)
-      this.setState({communities: this.sortCases('cases'), sortName: null, sortCase: 'desc'})
-    //   console.log("now sc is desc -->", this.state.sortCase)
-        // this.sortCases('cases')
+    } else if(this.state.sortCase === 'asc') {
+        this.setState({communities: this.sortCases('cases'), sortName: null, sortCase: 'desc'})
     }
   }
   
   render() {
-    //   console.log("render", this.state)
-
-    console.log("render", this.state.communities)
-    console.log("render", this.state.sortName)
-    console.log("render", this.state.sortCase)
     return (
         <table className="m-table">
         <thead>
@@ -127,8 +93,8 @@ class Drawer extends Component {
                 <th>communities in {this.state.name}</th>
             </tr>
           <tr>
-            <th onClick = {this.handleNameClick.bind(this)}>Name</th>
-            <th onClick = {this.handleCaseClick.bind(this)}>Cases</th>
+            <th onClick = {() => this.handleNameClick()}>Name</th>
+            <th onClick = {() => this.handleCaseClick()}>Cases</th>
           </tr>
         </thead>
         <tbody>
