2225a663caa964cdf2aa1f8ef14ed51e2a0611fd
jn9cn <jannine.chan@gmail.com>
2018-06-28 16:27:26 -0400

    material ui drawer works

--- src/Drawer.js
+++ src/Drawer.js
@@ -1,115 +1,64 @@
 import React, { Component } from 'react';
 import './App.css';
-import data from './data.js';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Drawer from '@material-ui/core/Drawer'
+import CollapsePanel from './CollapsePanel.svg'
+import Table from './Table.js'
 
-class Drawer extends Component {
+class SideDrawer extends Component {
   constuctor() {
-    // eslint-disable-next-line 
+    // eslint-disable-next-line
     this.state = {
-      name: '',
-      communities: [],
-      sortName: null, // null, 'asc', 'desc'
-      sortCase: null,
+        drawerOpen: true
     }
-    this.compareNames = this.compareNames.bind(this)
-    this.compareCases = this.compareCases.bind(this)
-    this.sortNames = this.sortNames.bind(this)
-    this.sortCases = this.sortCases.bind(this)
-  }
-
-  componentWillMount() {
-    this.setState({
-        name: data.name, 
-        communities: data.communities,
-        sortName: null, // null, 'asc', 'desc'
-        sortCase: null,
-    })
+    this.handleDrawerOpen = this.handleDrawerOpen.bind(this)
+    this.handleDrawerClose = this.handleDrawerClose.bind(this)
   }
 
   componentDidMount() {
-    this.setState({
-        name: data.name, 
-        communities: data.communities
-    })
-  }
-  
-  compareNames(name) {
-    if(this.state.sortName === 'asc') {
-        return (a,b) => {
-            if(a[name] < b[name]) return 1
-            if(a[name] > b[name]) return -1
-            return 0
-        }
-    } else {
-        return (a,b) => {
-            if(a[name] < b[name]) return -1
-            if(a[name] > b[name]) return 1
-            return 0
-        }
-    }
+    this.setState({ drawerOpen: true })
   }
 
-  compareCases(num) {
-    if(this.state.sortCase === 'asc') {
-        return (a,b) => b[num] - a[num] 
-    } else {
-        return (a,b) => a[num] - b[num]
-    }
+  toggle() {
+      if(!this.state.drawerOpen) this.handleDrawerOpen()
+      else this.handleDrawerClose()
   }
 
-  sortNames(key) {
-    let arrCopy = [...this.state.communities]
-    return arrCopy.sort(this.compareNames(key))
-  }
+  handleDrawerOpen = () => {
+      console.log("open!")
+    this.setState({ drawerOpen: true });
+  };
 
-  sortCases(key) {
-    let arrCopy = [...this.state.communities]
-    return arrCopy.sort(this.compareCases(key))
-  }
-
-  handleNameClick() {
-    if(this.state.sortName === null || this.state.sortName === 'desc') {
-        this.setState({communities: this.sortNames('name'), sortName: 'asc', sortCase: null})
-    } else if(this.state.sortName === 'asc') {
-        this.setState({communities: this.sortNames('name'), sortName: 'desc', sortCase: null})
-    }
-  }
-
-  handleCaseClick() {
-    if(this.state.sortCase === null || this.state.sortCase === 'desc') {
-        this.setState({communities: this.sortCases('cases'), sortName: null, sortCase: 'asc'})
-    } else if(this.state.sortCase === 'asc') {
-        this.setState({communities: this.sortCases('cases'), sortName: null, sortCase: 'desc'})
-    }
-  }
+  handleDrawerClose = () => {
+      console.log("closing!")
+    this.setState({ drawerOpen: false });
+  };
   
   render() {
+      console.log("render", this.state)
+    if (this.state === null ) return ( <div></div> )
     return (
-        <table className="m-table">
-        <thead>
-            <tr>
-                <th>List of Communities</th>
-                <th>{this.state.communities.length}</th>
-                <th>communities in {this.state.name}</th>
-            </tr>
-          <tr>
-            <th onClick = {() => this.handleNameClick()}>Name</th>
-            <th onClick = {() => this.handleCaseClick()}>Cases</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.communities.map(function(elem, index) {
-            return (
-              <tr key={index} data-item={elem}>
-                <td data-title="Name">{elem.name}</td>
-                <td data-title="Cases">{elem.cases}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+        <MuiThemeProvider>
+            <div style={{display: 'flex', justifyContent: 'flex-start'}}>
+            <img style={{height: '25px', width: '25px', transform: 'rotate(180deg)'}} 
+                    src={CollapsePanel} 
+                    alt="collapsePanel" 
+                    onClick={() => this.toggle()}
+                    />
+            </div>
+            <Drawer variant='temporary' open={this.state.drawerOpen}>
+                <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+                    <img style={{height: '25px', width: '25px'}} 
+                    src={CollapsePanel} 
+                    alt="collapsePanel" 
+                    onClick={() => this.toggle()}
+                    />
+                </div>
+                <Table />
+            </Drawer>
+        </MuiThemeProvider>
     );
   }
 }
 
-export default Drawer;
+export default SideDrawer;
---
+++ src/Table.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+import './App.css';
+import data from './data.js';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+class Table extends Component {
+  constuctor() {
+    // eslint-disable-next-line 
+    this.state = {
+      name: '',
+      communities: [],
+      sortName: null, // null, 'asc', 'desc'
+      sortCase: null,
+      drawerOpen: false
+    }
+    this.compareNames = this.compareNames.bind(this)
+    this.compareCases = this.compareCases.bind(this)
+    this.sortNames = this.sortNames.bind(this)
+    this.sortCases = this.sortCases.bind(this)
+  }
+
+  componentWillMount() {
+    this.setState({
+        name: data.name, 
+        communities: data.communities,
+        sortName: null, // null, 'asc', 'desc'
+        sortCase: null,
+        drawerOpen: false
+    })
+  }
+
+  componentDidMount() {
+    this.setState({
+        name: data.name, 
+        communities: data.communities
+    })
+  }
+  
+  compareNames(name) {
+    if(this.state.sortName === 'asc') {
+        return (a,b) => {
+            if(a[name] < b[name]) return 1
+            if(a[name] > b[name]) return -1
+            return 0
+        }
+    } else {
+        return (a,b) => {
+            if(a[name] < b[name]) return -1
+            if(a[name] > b[name]) return 1
+            return 0
+        }
+    }
+  }
+
+  compareCases(num) {
+    if(this.state.sortCase === 'asc') {
+        return (a,b) => b[num] - a[num] 
+    } else {
+        return (a,b) => a[num] - b[num]
+    }
+  }
+
+  sortNames(key) {
+    let arrCopy = [...this.state.communities]
+    return arrCopy.sort(this.compareNames(key))
+  }
+
+  sortCases(key) {
+    let arrCopy = [...this.state.communities]
+    return arrCopy.sort(this.compareCases(key))
+  }
+
+  handleNameClick() {
+    if(this.state.sortName === null || this.state.sortName === 'desc') {
+        this.setState({communities: this.sortNames('name'), sortName: 'asc', sortCase: null})
+    } else if(this.state.sortName === 'asc') {
+        this.setState({communities: this.sortNames('name'), sortName: 'desc', sortCase: null})
+    }
+  }
+
+  handleCaseClick() {
+    if(this.state.sortCase === null || this.state.sortCase === 'desc') {
+        this.setState({communities: this.sortCases('cases'), sortName: null, sortCase: 'asc'})
+    } else if(this.state.sortCase === 'asc') {
+        this.setState({communities: this.sortCases('cases'), sortName: null, sortCase: 'desc'})
+    }
+  }
+  
+  render() {
+    return (
+        <MuiThemeProvider>
+
+        <table className="m-table">
+        <thead>
+            <tr>
+                <th>List of Communities</th>
+                <th>{this.state.communities.length}</th>
+                <th>communities in {this.state.name}</th>
+            </tr>
+          <tr>
+            <th onClick = {() => this.handleNameClick()}>Name</th>
+            <th onClick = {() => this.handleCaseClick()}>Cases</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.communities.map(function(elem, index) {
+            return (
+              <tr key={index} data-item={elem}>
+                <td data-title="Name">{elem.name}</td>
+                <td data-title="Cases">{elem.cases}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default Table;
